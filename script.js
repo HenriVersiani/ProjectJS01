48be3eebaf529973dbbe57713569a8ed82741f44
HenriVersiani <108374876+HenriVersiani@users.noreply.github.com>
2022-09-16 15:38:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let numberOne = prompt("Insira o primeiro número")
+let numberTwo = prompt("Insira o segundo número")
+
+let addResult = Number(numberOne) + Number(numberTwo);
+let subResult = Number(numberOne) - Number(numberTwo);
+let multiResult = Number(numberOne) * Number(numberTwo);
+let divResult = Number(numberOne) / Number(numberTwo);
+let remResult = Number(numberOne) % Number(numberTwo);
+
+alert("A soma dos números é " + addResult)
+alert("A subtraçao dos números é " +subResult)
+alert("A multiplicaçao dos números é "+multiResult)
+alert("A divisao dos números é "+divResult)
+alert("O resto da divisao dos números é " + remResult)
